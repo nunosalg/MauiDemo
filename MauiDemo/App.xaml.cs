8964ec4d248ee06a063f901bc8dc4b14a283a058
nunosalg <nunosalgueiro23@gmail.com>
2024-09-30 10:33:17 +0100

    Add project files.

---
+++ MauiDemo/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MauiDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            var navPage = new NavigationPage(new MyPage());
+            
+            navPage.BarBackground = Colors.Yellow;
+            navPage.BarTextColor = Colors.Blue;
+
+            MainPage = new TabbedPageDemo();
+        }
+    }
+}
