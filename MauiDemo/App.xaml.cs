51fd5bf832c45ba34495e7fda6f952590a74a996
nunosalg <nunosalgueiro23@gmail.com>
2024-09-30 13:06:45 +0100

    Absolute layout page

--- MauiDemo/App.xaml.cs
+++ MauiDemo/App.xaml.cs
@@ -6,7 +6,7 @@
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new FlexLayoutDemo());
+            MainPage = new NavigationPage(new AbsoluteLayoutDemo());
         }
     }
 }
