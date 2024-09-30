b1fe162021994f4d6b092e3f0745f0dd5cf9815f
nunosalg <nunosalgueiro23@gmail.com>
2024-09-30 12:39:39 +0100

    Grid Layout page

--- MauiDemo/App.xaml.cs
+++ MauiDemo/App.xaml.cs
@@ -6,7 +6,7 @@
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new MyPage());
+            MainPage = new NavigationPage(new FlexLayoutDemo());
         }
     }
 }
