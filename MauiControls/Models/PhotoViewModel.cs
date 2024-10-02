b6624c745e0a7c1a8c5e6fab593222f4f2c5b19d
nunosalg <nunosalgueiro23@gmail.com>
2024-10-02 13:29:46 +0100

    Collection controls

---
+++ MauiControls/Models/PhotoViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiControls.Models
+{
+    public class PhotoViewModel
+    {
+        public ObservableCollection<Photo> photos { get; private set; }
+        public ObservableCollection<Photo> Photos
+        {
+            get { return photos; }
+            set { photos = value; }
+        }
+
+        public PhotoViewModel()
+        {
+            Photos = new ObservableCollection<Photo>()
+            {
+                new Photo() {Name = "Andromeda", ImageUrl = "andromeda.jpeg"},
+                new Photo() {Name = "Milky Way", ImageUrl = "milkyway.jpeg"},
+                new Photo() {Name = "Universe", ImageUrl = "universe.jpeg"},
+                new Photo() {Name = "Galaxy", ImageUrl = "galaxy.jpeg"},
+                new Photo() {Name = "Sombrero", ImageUrl = "galaxy5.jpeg"},
+                new Photo() {Name = "Andromeda", ImageUrl = "andromeda.jpeg"},
+                new Photo() {Name = "Milky Way", ImageUrl = "milkyway.jpeg"},
+                new Photo() {Name = "Universe", ImageUrl = "universe.jpeg"},
+                new Photo() {Name = "Galaxy", ImageUrl = "galaxy.jpeg"},
+                new Photo() {Name = "Sombrero", ImageUrl = "galaxy5.jpeg"},
+                new Photo() {Name = "Andromeda", ImageUrl = "andromeda.jpeg"},
+                new Photo() {Name = "Milky Way", ImageUrl = "milkyway.jpeg"},
+                new Photo() {Name = "Universe", ImageUrl = "universe.jpeg"},
+                new Photo() {Name = "Galaxy", ImageUrl = "galaxy.jpeg"},
+                new Photo() {Name = "Sombrero", ImageUrl = "galaxy5.jpeg"},
+            };
+        }
+    }
+}
